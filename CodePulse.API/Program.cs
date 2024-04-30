b6c87df9e7d48f7c289f326ac2c60b16f4c1fd0d
jaime.mtz17 <jaime.mtz17@gmail.com>
2024-04-30 17:15:04 -0600

    24. Database Created

--- CodePulse.API/Program.cs
+++ CodePulse.API/Program.cs
@@ -1,9 +1,16 @@
+using CodePulse.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+});
 
 var app = builder.Build();
 
