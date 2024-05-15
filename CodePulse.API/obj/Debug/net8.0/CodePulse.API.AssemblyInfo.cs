bf91e13c5518f226de4e5ee84850b2a0522a9e1c
Jaime <jaime.martinez@perficient.com>
2024-05-15 08:02:56 -0600

    add gitignore

--- CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
+++ CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ada2202829c25a15ed1b6c7049fda96aefa074")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23648865777efcf3123866bac37467cc6646dee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
